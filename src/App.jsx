b17681d1dec03aacf57f5d135b2583bd004b9a6d
asheq1 <bsmrstu18fmb025@gmail.com>
2024-09-02 15:34:31 +0600

    bottle cart

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import Watch from './components/Watch/Watch'
 import { useEffect } from 'react';
+import Header from './components/Header/Header';
+import Bottles from './components/Bottles/Bottles';
 
 function App() {
  
-  const watchs = [
-    {id: 1, name: 'canon', price: 3214},
-    {id: 2, name: 'Samsu', price:1234},
-    {id: 3, name: 'cassio', price: 1643},
-
-  ]
-
-  
-  // local
-  const [watches, setWatches] = useState([]);
-  useEffect(()=>{
-    fetch('watchs.json')
-    .then(res => res.json())
-    .then(data => setWatches(data))
-
-  },[])
- 
-
 
   return (
     <>
-      <h1>Data Storage Integration</h1>
-      {
-        watchs.map(watch => <Watch 
-         key={ watch.id}
-         watch={watch}></Watch>)
-      }
+      <Header></Header>
+      <Bottles></Bottles>
+
     </>
   )
 }
